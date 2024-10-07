d38f6c1f795cb07b57a40af5029e249cc6e06842
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2024-10-07 14:06:56 -0300

    tribuinal

--- code/index.py
+++ code/index.py
@@ -1,7 +1,9 @@
 from PySide6.QtWidgets import QMainWindow, QApplication
 from tkinter.filedialog import askopenfilename
 from window import Ui_MainWindow
-import pyautogui as pg
+from bs4 import BeautifulSoup 
+import requests 
+import abc
 import sys
 
 class Arquivo:
@@ -15,21 +17,42 @@ class Arquivo:
         self.caminho
         ...
 
+class Tribunal:
+    __metaclass__ = abc.ABCMeta
+    
+    @abc.abstractmethod
+    def infos(self, soap: BeautifulSoup):
+        raise NotImplementedError("Please Implement this method")
+    
+class STF(Tribunal):
+    def infos(soap:BeautifulSoup):
+        ...
+
 class Browser:
     def __init__(self) -> None:
+        self.ref_enderecos = {
+            'stf' : ['', STF()]
+        }
+
         pass
 
-    def abrir(self):
-        ...
+    def pesquisar(self, num_processo:str, tipo:str):
+        try:
+            endereco = self.ref_enderecos[tipo][0]
 
-    def pesquisa(self):
-        ...
+            # Send a GET request to the website 
+            response = requests.get(
+                endereco.replace({'{ num_proc }'}, num_processo))
+            
+            # Check if the request was successful 
+            html_content = response.text 
+                
+            # Parse the HTML content 
+            soap = BeautifulSoup(html_content, 'html.parser')
 
-    def inserir(dado:str):
-        ...
-    
-    def infos(self):
-        ...
+            self.ref_enderecos[tipo][1].infos(soap)
+        except: 
+            print(f"Failed to retrieve content: {response.status_code}") 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent = None):
--- docs/README.md
+++ docs/README.md
@@ -1,11 +1,24 @@
-# Traçar posição do mouse
+# PyaAtoGui
+
+## Traçar posição do mouse
 pg.mouseInfo()
 
-# Maximizar tela
+## Maximizar tela
 pg.hotkey('win' , 'up')
 
-# Pesquisa
+## Pesquisa
 pg.press('browsersearch')
 
-# Teclas teclado
+## Teclas teclado
 https://pyautogui.readthedocs.io/en/latest/keyboard.html 
+
+# Sites para pesquisa
+- https://pje-consulta-publica.tjmg.jus.br/
+- https://www.stj.jus.br/sites/portalp/Processos/Consulta-Processual
+  - https://processo.stj.jus.br/processo/pesquisa/?termo={numProc}&aplicacao=processos.ea&tipoPesquisa=tipoPesquisaGenerica&chkordem=DESC&chkMorto=MORTO
+- https://portal.stf.jus.br/processos/
+  - https://portal.stf.jus.br/processos/listarProcessos.asp?classe=&numeroProcesso={numProc}
+- https://consultaprocessual.tst.jus.br/consultaProcessual/
+  - https://consultaprocessual.tst.jus.br/consultaProcessual/consultaTstNumUnica.do;jsessionid=uxdjFRpmu-KyfKN86oPdErxdfnj-D0rJfcpRsBs6.consultaprocessual-36-5rdxp?conscsjt=&numeroTst={numProc}&digitoTst=2&anoTst=&orgaoTst=5&tribunalTst=&varaTst=&consulta=Consultar
+- https://www.tjmg.jus.br/portal-tjmg/processos/andamento-processual/
+  - com captcha
