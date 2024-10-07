c33119f06190ef4176a53a68d57cd3ebe37393f4
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2024-10-07 11:45:28 -0300

    window updt

--- code/index.py
+++ code/index.py
@@ -1,12 +1,46 @@
 from PySide6.QtWidgets import QMainWindow, QApplication
+from tkinter.filedialog import askopenfilename
 from window import Ui_MainWindow
 import pyautogui as pg
 import sys
+
+class Arquivo:
+    def __init__(self) -> None:
+        pass
+
+    def inserir(self):
+        caminho = askopenfilename()
+
+    def alterar(self):
+        self.caminho
+        ...
+
+class Browser:
+    def __init__(self) -> None:
+        pass
+
+    def abrir(self):
+        ...
+
+    def pesquisa(self):
+        ...
+
+    def inserir(dado:str):
+        ...
+    
+    def infos(self):
+        ...
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setupUi(self)
+
+    def loading(self):
+        ...
+
+    def buscar(self):
+        ...
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- code/window.py
+++ code/window.py
@@ -17,71 +17,103 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QGridLayout,
     QLabel, QLineEdit, QMainWindow, QPushButton,
-    QSizePolicy, QStatusBar, QWidget)
+    QSizePolicy, QStatusBar, QTabWidget, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(423, 279)
-        MainWindow.setMinimumSize(QSize(423, 279))
+        MainWindow.resize(478, 279)
+        MainWindow.setMinimumSize(QSize(478, 279))
+        MainWindow.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonIconOnly)
+        MainWindow.setDocumentMode(False)
+        MainWindow.setTabShape(QTabWidget.TabShape.Rounded)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout_2 = QGridLayout(self.centralwidget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
-
-        self.gridLayout_2.addWidget(self.pushButton, 1, 0, 1, 1)
-
         self.gridLayout = QGridLayout()
         self.gridLayout.setObjectName(u"gridLayout")
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy)
         font = QFont()
         font.setFamilies([u"Rockwell"])
-        font.setBold(True)
+        font.setPointSize(22)
+        font.setBold(False)
         self.label_3.setFont(font)
+        self.label_3.setScaledContents(True)
+        self.label_3.setAlignment(Qt.AlignmentFlag.AlignBottom|Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft)
+        self.label_3.setWordWrap(True)
+        self.label_3.setMargin(5)
 
         self.gridLayout.addWidget(self.label_3, 2, 1, 1, 2)
 
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setFont(font)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(39)
+        sizePolicy1.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy1)
+        font1 = QFont()
+        font1.setFamilies([u"Rockwell"])
+        font1.setPointSize(22)
+        font1.setWeight(QFont.Thin)
+        self.label_2.setFont(font1)
+        self.label_2.setAutoFillBackground(True)
+        self.label_2.setScaledContents(True)
+        self.label_2.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
+        self.label_2.setWordWrap(True)
+        self.label_2.setMargin(5)
 
-        self.gridLayout.addWidget(self.label_2, 5, 1, 1, 2)
+        self.gridLayout.addWidget(self.label_2, 4, 1, 1, 2)
 
         self.lineEdit = QLineEdit(self.centralwidget)
         self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setReadOnly(True)
 
-        self.gridLayout.addWidget(self.lineEdit, 6, 2, 1, 1)
+        self.gridLayout.addWidget(self.lineEdit, 5, 2, 1, 1)
 
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
-        font1 = QFont()
-        font1.setFamilies([u"Verdana"])
-        font1.setPointSize(14)
-        font1.setBold(False)
-        self.label.setFont(font1)
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Maximum)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy2)
+        font2 = QFont()
+        font2.setFamilies([u"Verdana"])
+        font2.setPointSize(22)
+        font2.setBold(True)
+        font2.setItalic(False)
+        font2.setStrikeOut(False)
+        font2.setKerning(True)
+        self.label.setFont(font2)
         self.label.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
         self.label.setTextFormat(Qt.TextFormat.AutoText)
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setWordWrap(True)
 
         self.gridLayout.addWidget(self.label, 0, 2, 1, 1)
 
-        self.line_2 = QFrame(self.centralwidget)
-        self.line_2.setObjectName(u"line_2")
-        self.line_2.setFrameShadow(QFrame.Shadow.Plain)
-        self.line_2.setLineWidth(4)
-        self.line_2.setMidLineWidth(2)
-        self.line_2.setFrameShape(QFrame.Shape.HLine)
+        self.line_1 = QFrame(self.centralwidget)
+        self.line_1.setObjectName(u"line_1")
+        self.line_1.setFrameShadow(QFrame.Shadow.Plain)
+        self.line_1.setLineWidth(4)
+        self.line_1.setMidLineWidth(2)
+        self.line_1.setFrameShape(QFrame.Shape.HLine)
 
-        self.gridLayout.addWidget(self.line_2, 1, 0, 1, 4)
+        self.gridLayout.addWidget(self.line_1, 1, 0, 1, 4)
 
         self.pushButton_2 = QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
 
-        self.gridLayout.addWidget(self.pushButton_2, 6, 1, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_2, 5, 1, 1, 1)
 
         self.comboBox = QComboBox(self.centralwidget)
         self.comboBox.setObjectName(u"comboBox")
@@ -90,31 +122,61 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.comboBox, 3, 1, 1, 2)
 
-        self.line = QFrame(self.centralwidget)
-        self.line.setObjectName(u"line")
-        self.line.setFrameShadow(QFrame.Shadow.Plain)
-        self.line.setLineWidth(4)
-        self.line.setMidLineWidth(2)
-        self.line.setFrameShape(QFrame.Shape.HLine)
+        self.line_3 = QFrame(self.centralwidget)
+        self.line_3.setObjectName(u"line_3")
+        self.line_3.setFrameShadow(QFrame.Shadow.Plain)
+        self.line_3.setLineWidth(4)
+        self.line_3.setMidLineWidth(2)
+        self.line_3.setFrameShape(QFrame.Shape.HLine)
 
-        self.gridLayout.addWidget(self.line, 4, 1, 1, 2)
+        self.gridLayout.addWidget(self.line_3, 6, 1, 1, 2)
+
+        self.logo = QLabel(self.centralwidget)
+        self.logo.setObjectName(u"logo")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.logo.sizePolicy().hasHeightForWidth())
+        self.logo.setSizePolicy(sizePolicy3)
+        self.logo.setMaximumSize(QSize(50, 16777215))
+        self.logo.setPixmap(QPixmap(u"../../../../../Downloads/conf-icon (1).ico"))
+        self.logo.setScaledContents(True)
+
+        self.gridLayout.addWidget(self.logo, 0, 1, 1, 1)
 
 
-        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 3, 1)
+
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        sizePolicy4 = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Fixed)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy4)
+        self.pushButton.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
+        self.pushButton.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.pushButton.setAutoDefault(False)
+        self.pushButton.setFlat(False)
+
+        self.gridLayout_2.addWidget(self.pushButton, 3, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusBar = QStatusBar(MainWindow)
         self.statusBar.setObjectName(u"statusBar")
+        self.statusBar.setStyleSheet(u"background-color: rgb(85, 170, 255);")
         MainWindow.setStatusBar(self.statusBar)
 
         self.retranslateUi(MainWindow)
+
+        self.pushButton.setDefault(True)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Executar", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Tipo de processo", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Relat\u00f3rio de processos", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Acompanha processos", None))
@@ -125,5 +187,10 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(accessibility)
         self.comboBox.setAccessibleDescription("")
 #endif // QT_CONFIG(accessibility)
+        self.logo.setText("")
+#if QT_CONFIG(accessibility)
+        self.pushButton.setAccessibleDescription("")
+#endif // QT_CONFIG(accessibility)
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Executar", None))
     # retranslateUi
 
