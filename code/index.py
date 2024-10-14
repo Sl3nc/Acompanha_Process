c0ca877e6527d13e23381b3c4fd6838fbf33b329
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2024-10-14 09:35:00 -0300

    src

--- code/index.py
+++ code/index.py
@@ -1,10 +1,27 @@
 from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtGui import QPixmap, QIcon
+from PySide6.QtCore import QSize
 from tkinter.filedialog import askopenfilename
-from window import Ui_MainWindow
-from bs4 import BeautifulSoup 
-import requests 
-import abc
+from src.window import Ui_MainWindow
+from pathlib import Path
+from time import sleep
+
 import sys
+import os
+from abc import abstractmethod, ABCMeta
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+def resource_path(relative_path):
+        base_path = getattr(
+            sys,
+            '_MEIPASS',
+            os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(base_path, relative_path)
 
 class Arquivo:
     def __init__(self) -> None:
@@ -17,47 +34,104 @@ class Arquivo:
         self.caminho
         ...
 
-class Tribunal:
-    __metaclass__ = abc.ABCMeta
-    
-    @abc.abstractmethod
-    def infos(self, soap: BeautifulSoup):
-        raise NotImplementedError("Please Implement this method")
-    
-class STF(Tribunal):
-    def infos(soap:BeautifulSoup):
-        ...
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
 
 class Browser:
-    def __init__(self) -> None:
-        self.ref_enderecos = {
-            'stf' : ['', STF()]
-        }
+    ROOT_FOLDER = Path(__file__).parent
+    CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
 
+    def __init__(self, options = '') -> None:
+        self.browser = self.make_chrome_browser(*options)
         pass
 
-    def pesquisar(self, num_processo:str, tipo:str):
-        try:
-            endereco = self.ref_enderecos[tipo][0]
+    def make_chrome_browser(self,*options: str) -> webdriver.Chrome:
+        chrome_options = webdriver.ChromeOptions()
 
-            # Send a GET request to the website 
-            response = requests.get(
-                endereco.replace({'{ num_proc }'}, num_processo))
-            
-            # Check if the request was successful 
-            html_content = response.text 
-                
-            # Parse the HTML content 
-            soap = BeautifulSoup(html_content, 'html.parser')
+        # chrome_options.add_argument('--headless')
+        if options is not None:
+            for option in options:
+                chrome_options.add_argument(option)
 
-            self.ref_enderecos[tipo][1].infos(soap)
-        except: 
-            print(f"Failed to retrieve content: {response.status_code}") 
+        chrome_service = Service(
+            executable_path=str(self.CHROME_DRIVER_PATH),
+        )
+
+        browser = webdriver.Chrome(
+            service=chrome_service,
+            options=chrome_options
+        )
+
+        return browser
+
+class Tribunal:
+    TIME_TO_WAIT = 10
+    __metaclass__ = ABCMeta
+
+    def __init__(self, link, option = '') -> None:
+        self.browser = Browser(option)
+        self.browser.get(link)
+        pass
+
+    @abstractmethod
+    def exec(self, num_processo):
+        raise NotImplementedError("Implemente este método")
+    
+class EPROC(Tribunal):
+    def __init__(self) -> None:
+        super().__init__('https://eproc1g.trf6.jus.br/eproc/externo_controlador.php?acao=processo_consulta_publica')
+        pass
+    
+    def exec(self):
+        ...
+
+class PJE(Tribunal):
+    CLASS_ELEMENTS = 'col-sm-12'
+
+    def __init__(self) -> None:
+        super().__init__('https://pje-consulta-publica.tjmg.jus.br/')
+        pass
+
+    def exec(self, num_processo):
+        self.browser.find_element(By.NAME, 
+            'fPP:numProcesso-inputNumeroProcessoDecoration:numProcesso-inputNumeroProcesso').send_keys(num_processo)
+
+
+        self.browser.find_element(By.NAME, 'fPP:searchProcessos').click()
+
+        sleep(self.TIME_TO_WAIT)
+
+        self.browser.find_element(By.CSS_SELECTOR,'#fPP\\:processosTable\\:632256959\\:j_id245 > a').click()
+
+class Juiz:
+    def __init__(self) -> None:
+        self.processos = {}
+
+        self.ref = {
+            'pje': PJE(),
+        }
+        pass
+
+    def add_processo(self, num:str, nome:str):
+        self.processos[num, self.__apurar(nome)]
+
+    def __apurar(self, nome:str):
+        for key, value in self.ref.items():
+            if nome == key:
+                return value 
+        raise Exception('Processo de tribunal não identificado')
+    
+    def pesquisar(self):
+        for num, tribunal in self.processos.items():
+            tribunal.exec(num)           
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent = None):
         super().__init__(parent)
         self.setupUi(self)
+
+        self.logo.setPixmap(QPixmap(resource_path("src\\imgs\\procc-icon.ico")))
+        # self.icon.addFile(resource_path("src\\imgs\\upload-icon.png"), QSize(), QIcon.Mode.Normal, QIcon.State.Off)
 
     def loading(self):
         ...
---
+++ code/src/window.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
+    QMainWindow, QPushButton, QSizePolicy, QStatusBar,
+    QTabWidget, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(478, 279)
+        MainWindow.setMinimumSize(QSize(478, 279))
+        MainWindow.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonIconOnly)
+        MainWindow.setDocumentMode(False)
+        MainWindow.setTabShape(QTabWidget.TabShape.Rounded)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.gridLayout = QGridLayout()
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.line_1 = QFrame(self.centralwidget)
+        self.line_1.setObjectName(u"line_1")
+        self.line_1.setFrameShadow(QFrame.Shadow.Plain)
+        self.line_1.setLineWidth(4)
+        self.line_1.setMidLineWidth(2)
+        self.line_1.setFrameShape(QFrame.Shape.HLine)
+
+        self.gridLayout.addWidget(self.line_1, 1, 1, 1, 3)
+
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy)
+        self.pushButton.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
+        self.pushButton.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.pushButton.setAutoDefault(False)
+        self.pushButton.setFlat(False)
+
+        self.gridLayout.addWidget(self.pushButton, 5, 3, 1, 1)
+
+        self.line_3 = QFrame(self.centralwidget)
+        self.line_3.setObjectName(u"line_3")
+        self.line_3.setFrameShadow(QFrame.Shadow.Plain)
+        self.line_3.setLineWidth(4)
+        self.line_3.setMidLineWidth(2)
+        self.line_3.setFrameShape(QFrame.Shape.HLine)
+
+        self.gridLayout.addWidget(self.line_3, 5, 1, 1, 2)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy1)
+        font = QFont()
+        font.setFamilies([u"Verdana"])
+        font.setPointSize(22)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setStrikeOut(False)
+        font.setKerning(True)
+        self.label.setFont(font)
+        self.label.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
+        self.label.setTextFormat(Qt.TextFormat.AutoText)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setWordWrap(True)
+
+        self.gridLayout.addWidget(self.label, 0, 2, 1, 2)
+
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        sizePolicy1.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
+        self.pushButton_2.setSizePolicy(sizePolicy1)
+        icon = QIcon()
+        icon.addFile(u"../imgs/upload-icon.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.pushButton_2.setIcon(icon)
+        self.pushButton_2.setIconSize(QSize(75, 75))
+
+        self.gridLayout.addWidget(self.pushButton_2, 4, 1, 1, 3)
+
+        self.logo = QLabel(self.centralwidget)
+        self.logo.setObjectName(u"logo")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Preferred)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.logo.sizePolicy().hasHeightForWidth())
+        self.logo.setSizePolicy(sizePolicy2)
+        self.logo.setMaximumSize(QSize(100, 100))
+        self.logo.setPixmap(QPixmap(u"../imgs/procc-icon.ico"))
+        self.logo.setScaledContents(True)
+
+        self.gridLayout.addWidget(self.logo, 0, 1, 1, 1)
+
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Minimum)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy3)
+        font1 = QFont()
+        font1.setFamilies([u"Arial Rounded MT"])
+        font1.setPointSize(12)
+        font1.setBold(True)
+        font1.setItalic(False)
+        self.label_2.setFont(font1)
+        self.label_2.setScaledContents(False)
+        self.label_2.setWordWrap(False)
+
+        self.gridLayout.addWidget(self.label_2, 2, 1, 1, 2)
+
+
+        self.gridLayout_2.addLayout(self.gridLayout, 3, 0, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusBar = QStatusBar(MainWindow)
+        self.statusBar.setObjectName(u"statusBar")
+        self.statusBar.setStyleSheet(u"background-color: rgb(85, 170, 255);")
+        MainWindow.setStatusBar(self.statusBar)
+
+        self.retranslateUi(MainWindow)
+
+        self.pushButton.setDefault(True)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+#if QT_CONFIG(accessibility)
+        self.pushButton.setAccessibleDescription("")
+#endif // QT_CONFIG(accessibility)
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Executar", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Acompanha processos", None))
+        self.pushButton_2.setText("")
+        self.logo.setText("")
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Insira aqui seu relat\u00f3rio de processos", None))
+    # retranslateUi
+
